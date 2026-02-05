0c52b801ebeb0b1bce0c9991420a4e3585a5f5c0
Aranghat Tech <aranghat@outlook.com>
2026-02-05 19:27:52 +0530

    Added Upload method

--- PhotoSharingApp/app/camera/index.tsx
+++ PhotoSharingApp/app/camera/index.tsx
@@ -1,7 +1,10 @@
 import {View, Text, Button, Image} from "react-native";
 import {useEffect, useState} from "react";
 import *  as ImagePicker from 'expo-image-picker';
-    
+import {ref, uploadBytes, getDownloadURL} from "firebase/storage";
+import firebase from "firebase/compat/app";
+import { storage} from "../../firebaseconfig";
+
 export default function Index(){
     
    const [image, setImage] = useState<string | undefined>();
@@ -20,6 +23,15 @@ export default function Index(){
             console.log("Picture taken:");
             console.log(result.assets[0].uri);
             setImage(result.assets[0].uri);
+            
+            //Upload the image to the firebase storage
+            const path = "images/" + Date.now() + ".jpg";
+            const response = await fetch(result.assets[0].uri);
+            const blob = await response.blob();
+            const storageRef = ref(storage, path);
+            await uploadBytes(storageRef, blob);
+            const url = await getDownloadURL(storageRef);
+            console.log(url);
         }
         else {
             console.log("Picture canceled");
